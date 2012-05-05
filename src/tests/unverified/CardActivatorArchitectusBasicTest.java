d75ba42c378ea850f25f9f326dc4ab15c250699c
Matthew Moss <mdm@cse.unsw.edu.au>
2012-05-05 16:52:55 +1000

    Changed Architectus activation order
    The card now correctly calls complete() immediately, and places a
    series of cards. The activation ends when the player places a
    non-architectus card.

--- src/tests/unverified/CardActivatorArchitectusBasicTest.java
+++ src/tests/unverified/CardActivatorArchitectusBasicTest.java
@@ -12,9 +12,9 @@ import framework.interfaces.MoveMaker;
 import framework.interfaces.activators.ArchitectusActivator;
 
 /**
- * 
+ *
  * Test the basic functionality of Architectus
- * 
+ *
  * @author Junjie CHEN
  *
  */
@@ -29,9 +29,9 @@ public class CardActivatorArchitectusBasicTest extends Test {
     @Override
     public void run(GameState gameState, MoveMaker move) throws AssertionError,
             UnsupportedOperationException, IllegalArgumentException {
-        
+
         gameState.setWhoseTurn(0);
-        
+
         Collection<Card> hand = new ArrayList<Card>();
         hand.add(Card.ARCHITECTUS);
         hand.add(Card.TURRIS);
@@ -39,85 +39,85 @@ public class CardActivatorArchitectusBasicTest extends Test {
         hand.add(Card.FORUM);
         hand.add(Card.LEGAT);
         hand.add(Card.ONAGER);
-        
+
         gameState.setPlayerHand(0, hand);
-                
+
         //no cards on the field
         Card[] field = new Card[Rules.NUM_DICE_DISCS];
         for(int i = 0; i < field.length; i++) {
             field[i] = Card.NOT_A_CARD;
         }
-        
+
         //player has enough Sestertiis to lay cards
         gameState.setPlayerSestertii(0, 30);
-        
+
         gameState.setActionDice(new int[] {1,4,5});
-        
+
         //===================== test1 ======================
         move.placeCard(Card.ARCHITECTUS, Rules.DICE_DISC_1);
-        
+
         //the correct amount of money should be deducted
         //game removed from hand and get placed on the field
         assert(gameState.getPlayerSestertii(0) == 27);
         assert(gameState.getPlayerHand(0).size() == 5);
         assert(!gameState.getPlayerHand(0).contains(Card.ARCHITECTUS));
-        
+
         field = gameState.getPlayerCardsOnDiscs(0);
         assert(field[0] == Card.ARCHITECTUS);
-        
+
         //if you lay a building card now, it should still cost you money
         move.placeCard(Card.FORUM, Rules.DICE_DISC_3);
         assert(gameState.getPlayerSestertii(0) == 22);
         assert(gameState.getPlayerHand(0).size() == 4);
         assert(!gameState.getPlayerHand(0).contains(Card.FORUM));
-        
+
         field = gameState.getPlayerCardsOnDiscs(0);
         assert(field[2] == Card.FORUM);
-        
+
         //activate Architectus
         ArchitectusActivator activator = (ArchitectusActivator) move.chooseCardToActivate(Rules.DICE_DISC_1);
-        
+        // The activation takes no parameters and can now be completed
+        activator.complete();
+
         //should be free to lay card now
         move.placeCard(Card.MACHINA, Rules.DICE_DISC_2);
-        
+
         //the money should not change
         assert(gameState.getPlayerSestertii(0) == 22);
         assert(gameState.getPlayerHand(0).size() == 3);
         assert(!gameState.getPlayerHand(0).contains(Card.MACHINA));
-        
+
         field = gameState.getPlayerCardsOnDiscs(0);
         assert(field[1] == Card.MACHINA);
-        
+
         //covered the forum
         move.placeCard(Card.TURRIS, Rules.DICE_DISC_3);
         assert(gameState.getPlayerSestertii(0) == 22);
         assert(gameState.getPlayerHand(0).size() == 2);
         assert(!gameState.getPlayerHand(0).contains(Card.TURRIS));
-        
+
         field = gameState.getPlayerCardsOnDiscs(0);
         assert(field[2] == Card.TURRIS);
-        
+
         //forum should be in the discard pile
         List<Card> discard = gameState.getDiscard();
         assert(discard.contains(Card.FORUM));
-        
+
         //laying character card should still cost you money
         move.placeCard(Card.LEGAT, Rules.DICE_DISC_5);
         assert(gameState.getPlayerSestertii(0) == 17);
         assert(gameState.getPlayerHand(0).size() == 1);
         assert(!gameState.getPlayerHand(0).contains(Card.LEGAT));
-        
+
         field = gameState.getPlayerCardsOnDiscs(0);
         assert(field[4] == Card.LEGAT);
-        
-        activator.complete();
-        
-        //after the activation, should cost you money again
+
+        //Activation is ended when something other than placing a building occurs
         move.placeCard(Card.ONAGER, Rules.DICE_DISC_6);
         assert(gameState.getPlayerSestertii(0) == 12);
-        assert(gameState.getPlayerHand(0).size() == 0);
+        assert(gameState.getPlayerHand(0).isEmpty());
         assert(!gameState.getPlayerHand(0).contains(Card.ONAGER));
-        
+
         field = gameState.getPlayerCardsOnDiscs(0);
         assert(field[5] == Card.ONAGER);
     }
