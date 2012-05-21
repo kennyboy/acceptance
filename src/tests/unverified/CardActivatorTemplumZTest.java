9990794aba14c4ca37c6f3cef33990cc0eee140d
Upload Form <mzhou.acceptanceupload@cse.unsw.edu.au>
2012-05-21 12:42:59 +1000

    Update by peter.xu

--- src/tests/unverified/CardActivatorTemplumZTest.java
+++ src/tests/unverified/CardActivatorTemplumZTest.java
@@ -98,6 +98,7 @@ public class CardActivatorTemplumZTest extends Test {
 		//ATTACK THE TEMPLUM
 		CenturioActivator ca = (CenturioActivator)move.chooseCardToActivate(5);
 		ca.giveAttackDieRoll(1);
+		ca.chooseCenturioAddActionDie(false);
 		ca.complete();
 		//SHOULD NOT KILL THE TEMPLUM
 		assert(gameState.getPlayerCardsOnDiscs(0)[4] == Card.TEMPLUM);
@@ -139,6 +140,7 @@ public class CardActivatorTemplumZTest extends Test {
 		//ATTACK THE TEMPLUM
 		ca = (CenturioActivator)move.chooseCardToActivate(5);
 		ca.giveAttackDieRoll(1);
+		ca.chooseCenturioAddActionDie(false);
 		ca.complete();
 		//SHOULD NOT KILL THE TEMPLUM
 		assert(gameState.getPlayerCardsOnDiscs(0)[4] == Card.TEMPLUM);
@@ -172,6 +174,7 @@ public class CardActivatorTemplumZTest extends Test {
 		//ATTACK THE TEMPLUM
 		ca = (CenturioActivator)move.chooseCardToActivate(5);
 		ca.giveAttackDieRoll(2);
+		ca.chooseCenturioAddActionDie(false);
 		ca.complete();
 		//SHOULD KILL THE TEMPLUM
 		assert(gameState.getPlayerCardsOnDiscs(0)[4] == Card.NOT_A_CARD);
